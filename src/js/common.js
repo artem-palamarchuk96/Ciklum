c01f73898e499746d0724eced878707ab6195a13
a.palamarchuk <artem.palamarchuk.96@gmail.com>
2016-07-17 02:52:21 +0300

    add media for dropbox

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,14 @@
 //= ../../bower_components/jquery/dist/jquery.js
 
 //= partials/ms.js
+
+// $(window).load(function() {
+
+// 	$(window).resize(function () {
+// 		$('dropbox-bg-wrap').css({
+// 			"background-position" : "100% "
+// 		})
+// 	})
+
+	
+// })
